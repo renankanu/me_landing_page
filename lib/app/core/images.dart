6d516d1254621c3e707946d9352b7b73d3d45587
renankanu <renankanu@gmail.com>
2022-03-02 09:17:36 -0300

    WIP

--- lib/app/core/images.dart
+++ lib/app/core/images.dart
@@ -3,7 +3,4 @@ abstract class BaseImages {
   static const String icGithub = 'assets/icons/ic_github.png';
   static const String me = 'assets/images/me.png';
   static const String dots = 'assets/images/dots.png';
-  static const String roundedWave = 'assets/images/rounded_wave.png';
-  static const String um = 'assets/images/rounded_wave.png';
-  static const String abstractImage = 'assets/images/abstract_image.png';
 }
