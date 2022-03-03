f586cc9b308dd5d73bf2471996a713c1de12498a
renankanu <renankanu@gmail.com>
2022-03-02 22:23:16 -0300

    feaature: create icons

--- lib/app/core/images.dart
+++ lib/app/core/images.dart
@@ -3,4 +3,8 @@ abstract class BaseImages {
   static const String icGithub = 'assets/icons/ic_github.png';
   static const String me = 'assets/images/me.png';
   static const String dots = 'assets/images/dots.png';
+  static const String icHome = 'assets/icons/ic_home.png';
+  static const String icSkill = 'assets/icons/ic_skill.png';
+  static const String icRepo = 'assets/icons/ic_repo.png';
+  static const String icExperience = 'assets/icons/ic_experience.png';
 }
--- lib/app/modules/home/views/widgets/left_menu.dart
+++ lib/app/modules/home/views/widgets/left_menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:me_landing_page/app/core/colors.dart';
+import 'package:me_landing_page/app/core/images.dart';
 
 class LeftMenu extends StatelessWidget {
   const LeftMenu({Key? key}) : super(key: key);
@@ -15,6 +17,65 @@ class LeftMenu extends StatelessWidget {
           topRight: Radius.circular(20),
           bottomLeft: Radius.circular(20),
           bottomRight: Radius.circular(20),
+        ),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Text(
+              'RK;',
+              style: Get.textTheme.headline4!.copyWith(
+                color: BaseColors.burntSienna,
+              ),
+            ),
+          ),
+          Divider(
+            color: BaseColors.burntSienna,
+            thickness: 1,
+            height: 1,
+          ),
+          MenuButton(
+            assetIcon: BaseImages.icHome,
+          ),
+          MenuButton(
+            assetIcon: BaseImages.icSkill,
+          ),
+          MenuButton(
+            assetIcon: BaseImages.icRepo,
+          ),
+          MenuButton(
+            assetIcon: BaseImages.icExperience,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MenuButton extends StatelessWidget {
+  const MenuButton({
+    Key? key,
+    required this.assetIcon,
+  }) : super(key: key);
+
+  final String assetIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 60,
+      decoration: BoxDecoration(
+        color: BaseColors.burntSienna,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Center(
+        child: Image.asset(
+          assetIcon,
+          height: 36,
+          width: 36,
+          color: Colors.white,
         ),
       ),
     );
