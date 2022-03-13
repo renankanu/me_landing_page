62befc832241b4586ebcd2a73289f06196ad2ed6
renankanu <renankanu@gmail.com>
2022-03-13 14:45:18 -0300

    refactor: base container responsive

--- lib/app/core/widgets/base_container.dart
+++ lib/app/core/widgets/base_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../core.dart';
 
@@ -21,19 +22,22 @@ class BaseContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
-    return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: _getHorizontalPadding(context),
-        vertical: 64,
-      ),
-      color: backgroundColor ?? Colors.transparent,
-      constraints: BoxConstraints(
-        minHeight: height * 0.9,
-        maxWidth: 1440,
-        minWidth: 1440,
-      ),
-      child: child,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: _getHorizontalPadding(context),
+            vertical: 64,
+          ),
+          color: backgroundColor ?? Colors.transparent,
+          constraints: BoxConstraints(
+            minHeight: Get.height * 0.9,
+            maxWidth: 1440,
+            minWidth: 1440,
+          ),
+          child: child,
+        );
+      },
     );
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -18,47 +18,49 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        key: _scaffoldKey,
-        backgroundColor: BaseColors.vistaWhite,
-        drawerEnableOpenDragGesture: false,
-        appBar: !Responsive.isDesktop()
-            ? AppBar(
-                backgroundColor: BaseColors.ebonyClay,
-                elevation: 0,
-                leading: IconButton(
-                  icon: Image.asset(
-                    BaseImages.icMenu,
-                    width: 24,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          key: _scaffoldKey,
+          backgroundColor: BaseColors.vistaWhite,
+          drawerEnableOpenDragGesture: false,
+          appBar: !Responsive.isDesktop()
+              ? AppBar(
+                  backgroundColor: BaseColors.ebonyClay,
+                  elevation: 0,
+                  leading: IconButton(
+                    icon: Image.asset(
+                      BaseImages.icMenu,
+                      width: 24,
+                    ),
+                    onPressed: () {
+                      _scaffoldKey.currentState!.openDrawer();
+                    },
                   ),
-                  onPressed: () {
-                    _scaffoldKey.currentState!.openDrawer();
-                  },
+                )
+              : null,
+          body: Stack(
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  children: const [
+                    AboutMe(),
+                    Skills(),
+                    Repo(),
+                    Experience(),
+                    Footer(),
+                  ],
                 ),
-              )
-            : null,
-        body: Stack(
-          children: [
-            SingleChildScrollView(
-              child: Column(
-                children: const [
-                  AboutMe(),
-                  Skills(),
-                  Repo(),
-                  Experience(),
-                  Footer(),
-                ],
               ),
-            ),
-            Visibility(
-              visible: Responsive.isDesktop(),
-              child: BaseSlideAnimation(child: LeftMenu()),
-            ),
-          ],
-        ),
-        drawer: LeftMenu(),
-      );
-    });
+              Visibility(
+                visible: Responsive.isDesktop(),
+                child: BaseSlideAnimation(child: LeftMenu()),
+              ),
+            ],
+          ),
+          drawer: LeftMenu(),
+        );
+      },
+    );
   }
 }
