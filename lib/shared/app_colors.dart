afd011f29066da817817839482940e78ecedf173
renankanu <renankanu@gmail.com>
2023-02-16 09:04:25 -0300

    feature: create buttons

--- lib/shared/app_colors.dart
+++ lib/shared/app_colors.dart
@@ -4,4 +4,5 @@ abstract class AppColors {
   static const Color ebonyClay = Color(0xFF1F2331);
   static const Color blueChill = Color(0xFF0D9488);
   static const Color riverBed = Color(0xFF4b5563);
+  static const Color boulder = Color(0xFF757575);
 }
--- lib/shared/widgets/social_button.dart
+++ lib/shared/widgets/social_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SocialButton extends StatelessWidget {
+class SocialButton extends StatefulWidget {
   const SocialButton({
     Key? key,
     required this.buttonColor,
@@ -14,57 +14,85 @@ class SocialButton extends StatelessWidget {
   final String name;
 
   @override
+  State<SocialButton> createState() => _SocialButtonState();
+}
+
+class _SocialButtonState extends State<SocialButton> {
+  late Color _hoverColor;
+
+  @override
+  void initState() {
+    _hoverColor = widget.buttonColor;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 205.08,
-      height: 48,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: shadowColor,
-            blurRadius: 6,
-            offset: const Offset(0, 4),
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onHover: (event) {
+        setState(() {
+          _hoverColor = widget.buttonColor.withOpacity(0.8);
+        });
+      },
+      onExit: (event) {
+        setState(() {
+          _hoverColor = widget.buttonColor;
+        });
+      },
+      child: GestureDetector(
+        child: Container(
+          width: 206,
+          height: 48,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: widget.shadowColor,
+                blurRadius: 6,
+                offset: const Offset(0, 4),
+              ),
+              BoxShadow(
+                color: widget.shadowColor,
+                blurRadius: 15,
+                offset: const Offset(0, 10),
+              ),
+            ],
+            color: _hoverColor,
           ),
-          BoxShadow(
-            color: shadowColor,
-            blurRadius: 15,
-            offset: const Offset(0, 10),
-          ),
-        ],
-        color: buttonColor,
-      ),
-      padding: const EdgeInsets.all(12),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(name,
-              style: GoogleFonts.poppins(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              )),
-          const SizedBox(width: 6),
-          SizedBox(
-            width: 22,
-            height: 22,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 22,
-                  height: 22,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: const FlutterLogo(size: 22),
+          padding: const EdgeInsets.all(12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(widget.name,
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  )),
+              const SizedBox(width: 6),
+              SizedBox(
+                width: 22,
+                height: 22,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 22,
+                      height: 22,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: const FlutterLogo(size: 22),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
