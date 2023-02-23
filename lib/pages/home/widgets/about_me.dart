5973ac847d8372c7c7c2f84728a4fc5c960436b6
renankanu <renankanu@gmail.com>
2023-02-23 17:55:23 -0300

    feature: added adaptive laptop in work section

--- lib/pages/home/widgets/about_me.dart
+++ lib/pages/home/widgets/about_me.dart
@@ -16,32 +16,151 @@ class AboutMe extends StatelessWidget {
     final width = MediaQuery.of(context).size.width;
     final padding = width * 0.03;
     return LayoutBuilder(builder: (_, constraints) {
-      if (Responsive.isMobile(context)) {
-        return Container(
-          height: 100,
-          width: 100,
-          color: Colors.green,
-        );
-      }
-      if (Responsive.isTablet(context)) {
-        return Container(
-          height: 100,
-          width: 100,
-          color: Colors.red,
-        );
-      }
       return Stack(
         children: [
           Padding(
-              padding: EdgeInsets.fromLTRB(padding, 140, padding, 0),
+            padding: EdgeInsets.fromLTRB(padding, 140, padding, 0),
+            child: Image(
+              image: const AssetImage(
+                AppImages.bg,
+              ),
+              fit: BoxFit.none,
+              height: 460,
+              width: constraints.maxWidth,
+              color: Colors.grey[700]!.withOpacity(0.2),
+              excludeFromSemantics: true,
+              frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+                if (wasSynchronouslyLoaded) {
+                  return child;
+                } else {
+                  return AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 300),
+                    child: frame != null ? child : const SizedBox.shrink(),
+                  );
+                }
+              },
+            ),
+          ),
+          Center(
+            child: Container(
+              constraints: const BoxConstraints(maxWidth: 1200),
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(padding, 160, 8, 0),
+                child: Visibility(
+                  visible: Responsive.isDesktop(context),
+                  replacement: Column(
+                    children: [
+                      MyAvatar(padding: padding),
+                      const MyDescription(),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const MyDescription(),
+                      MyAvatar(padding: padding),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      );
+    });
+  }
+}
+
+class MyDescription extends StatelessWidget {
+  const MyDescription({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SelectableText(
+          'Renan Santos',
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 80,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SelectableText(
+          'Flutter Developer',
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 20),
+        SelectableText(
+          '''Sou desenvolvedor na Megaleios, sou de Cianorte-PR.
+Trabalho com desenvolvimento desde 2016,
+conheço algumas tecnologias mas hoje estou focado em Flutter.''',
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+        ),
+        const SizedBox(height: 40),
+        Row(
+          children: [
+            SocialButton(
+              name: 'GitHub',
+              buttonColor: AppColors.riverBed,
+              icon: AppImages.github,
+              onTap: () async {
+                final url = Uri.parse('https://github.com/renankanu');
+                if (!await launchUrl(url)) {
+                  throw Exception('Could not launch $url');
+                }
+              },
+            ),
+            const SizedBox(width: 20),
+            SocialButton(
+              name: 'LinkedIn',
+              buttonColor: AppColors.blueChill,
+              icon: AppImages.linkedin,
+              onTap: () async {
+                final url =
+                    Uri.parse('https://www.linkedin.com/in/renansantosbr/');
+                if (!await launchUrl(url)) {
+                  throw Exception('Could not launch $url');
+                }
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class MyAvatar extends StatelessWidget {
+  const MyAvatar({
+    super.key,
+    required this.padding,
+  });
+
+  final double padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: padding),
+      child: AvatarAnimation(
+        child: Stack(
+          children: [
+            ClipOval(
               child: Image(
                 image: const AssetImage(
-                  AppImages.bg,
+                  AppImages.renanFour,
                 ),
-                fit: BoxFit.none,
-                height: 460,
-                width: constraints.maxWidth,
-                color: Colors.grey[700]!.withOpacity(0.2),
                 excludeFromSemantics: true,
                 frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
                   if (wasSynchronouslyLoaded) {
@@ -53,127 +172,22 @@ class AboutMe extends StatelessWidget {
                     );
                   }
                 },
-              )),
-          Center(
-            child: Container(
-              constraints: const BoxConstraints(maxWidth: 1200),
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(padding, 160, 8, 0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SelectableText(
-                            'Renan Santos',
-                            style: GoogleFonts.poppins(
-                              color: Colors.white,
-                              fontSize: 80,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SelectableText(
-                            'Flutter Developer',
-                            style: GoogleFonts.poppins(
-                              color: Colors.white,
-                              fontSize: 50,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          SelectableText(
-                            '''Sou desenvolvedor na Megaleios, sou de Cianorte-PR.
-Trabalho com desenvolvimento desde 2016,
-conheço algumas tecnologias mas hoje estou focado em Flutter.''',
-                            style: GoogleFonts.poppins(
-                              color: Colors.white,
-                              fontSize: 18,
-                            ),
-                          ),
-                          const SizedBox(height: 40),
-                          Row(
-                            children: [
-                              SocialButton(
-                                name: 'GitHub',
-                                buttonColor: AppColors.riverBed,
-                                icon: AppImages.github,
-                                onTap: () async {
-                                  final url =
-                                      Uri.parse('https://github.com/renankanu');
-                                  if (!await launchUrl(url)) {
-                                    throw Exception('Could not launch $url');
-                                  }
-                                },
-                              ),
-                              const SizedBox(width: 20),
-                              SocialButton(
-                                name: 'LinkedIn',
-                                buttonColor: AppColors.blueChill,
-                                icon: AppImages.linkedin,
-                                onTap: () async {
-                                  final url = Uri.parse(
-                                      'https://www.linkedin.com/in/renansantosbr/');
-                                  if (!await launchUrl(url)) {
-                                    throw Exception('Could not launch $url');
-                                  }
-                                },
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: padding),
-                      child: AvatarAnimation(
-                        child: Stack(
-                          children: [
-                            ClipOval(
-                              child: Image(
-                                image: const AssetImage(
-                                  AppImages.renanFour,
-                                ),
-                                excludeFromSemantics: true,
-                                frameBuilder: (context, child, frame,
-                                    wasSynchronouslyLoaded) {
-                                  if (wasSynchronouslyLoaded) {
-                                    return child;
-                                  } else {
-                                    return AnimatedSwitcher(
-                                      duration:
-                                          const Duration(milliseconds: 300),
-                                      child: frame != null
-                                          ? child
-                                          : const SizedBox.shrink(),
-                                    );
-                                  }
-                                },
-                              ),
-                            ),
-                            Container(
-                              height: 408,
-                              width: 408,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(300),
-                                border: Border.all(
-                                  color: AppColors.blueChill,
-                                  width: 6,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+              ),
+            ),
+            Container(
+              height: 408,
+              width: 408,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(300),
+                border: Border.all(
+                  color: AppColors.blueChill,
+                  width: 6,
                 ),
               ),
             ),
-          )
-        ],
-      );
-    });
+          ],
+        ),
+      ),
+    );
   }
 }
