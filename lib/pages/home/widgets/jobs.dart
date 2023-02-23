6a3b3c653918948d085afd0377ce0f3a430f3551
renankanu <renankanu@gmail.com>
2023-02-23 17:45:01 -0300

    feature: added adaptive laptop in work section

--- lib/pages/home/widgets/jobs.dart
+++ lib/pages/home/widgets/jobs.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:me_landing_page/shared/app_colors.dart';
+import 'package:me_landing_page/shared/utils/app_responsive.dart';
 import 'package:me_landing_page/shared/widgets/app_title_section.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
@@ -42,152 +43,55 @@ class Jobs extends StatelessWidget {
                         started: started,
                       ),
                       const SizedBox(height: 40),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Text(
-                                  'Aqui está o resumo de minha experiência profissional.',
-                                  style: GoogleFonts.poppins(
-                                    color: Colors.white.withOpacity(0.7),
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 16,
-                                  ),
+                      if (Responsive.isMobile(context))
+                        Column(
+                          children: const [
+                            Work(),
+                            SizedBox(height: 40),
+                            Knowledge(),
+                          ],
+                        )
+                      else
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Expanded(
+                              child: Work(),
+                            )
+                                .animate(
+                                  target: started ? 1.0 : 0.0,
+                                  onPlay: (controller) {
+                                    if (started) {
+                                      controller.forward();
+                                    }
+                                  },
+                                )
+                                .moveX(
+                                  begin: -100,
+                                  end: 0,
+                                  duration: 1.seconds,
+                                  curve: Curves.easeInOut,
                                 ),
-                                const SizedBox(height: 40),
-                                Stack(
-                                  children: [
-                                    Positioned(
-                                      top: 0,
-                                      left: 4,
-                                      bottom: 0,
-                                      child: Container(
-                                        width: 2,
-                                        decoration: const BoxDecoration(
-                                          color: AppColors.blueChill,
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color: AppColors.blueChill,
-                                              blurRadius: 4,
-                                              spreadRadius: 2,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 10),
-                                      child: Column(
-                                        children: const [
-                                          ItemWorkKnowledge(
-                                            title:
-                                                'Megaleios - 2019 até o momento',
-                                            description:
-                                                '- Desenvolvimento com Flutter; \n- Modular(gerência de dependências, rotas); \n- Bloc, RxDart, GetX; \n- Integração com Firebase; \n- Integração com OneSignal; \n- Figma; \n- Publicar App nas lojas(Apple e GooglePlay); \n- CD com Bitrise; \n- Integração com APIs REST; \n-Clean Architecture; \n-TDD; \n- Visionamento de código com git; \n- Desenvolvimento com React Native;',
-                                          ),
-                                          ItemWorkKnowledge(
-                                            title: 'Sisterra - 2015 até 2019',
-                                            description:
-                                                '- Desenvolvimento de aplicativos para Android nativo (Java e Kotlin); \n- Desenvolvimento de aplicações em PHP utilizando DDD para back-end; \n- APIs REST; \n- CQRS pattern; \n- Conhecimento em Docker; \n- Publicar App nas lojas(Apple e GooglePlay); \n- Conhecimento em AWS (Lambda, SNS, SQS, SES, EC2, RDS, S3); \n- Integração com APIs REST; \n- Usuário Linux (GNU / Linux Ubuntu);',
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
+                            const SizedBox(width: 40),
+                            const Expanded(
+                              child: Knowledge(),
+                            )
+                                .animate(
+                                  target: started ? 1.0 : 0.0,
+                                  onPlay: (controller) {
+                                    if (started) {
+                                      controller.forward();
+                                    }
+                                  },
+                                )
+                                .moveX(
+                                  begin: 100,
+                                  end: 0,
+                                  duration: 1.seconds,
+                                  curve: Curves.easeInOut,
                                 ),
-                              ],
-                            ),
-                          )
-                              .animate(
-                                target: started ? 1.0 : 0.0,
-                                onPlay: (controller) {
-                                  if (started) {
-                                    controller.forward();
-                                  }
-                                },
-                              )
-                              .moveX(
-                                begin: -100,
-                                end: 0,
-                                duration: 1.seconds,
-                                curve: Curves.easeInOut,
-                              ),
-                          const SizedBox(width: 40),
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Text(
-                                  'Aqui está o resumo de minha formação acadêmica e cursos.',
-                                  style: GoogleFonts.poppins(
-                                    color: Colors.white.withOpacity(0.7),
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                const SizedBox(height: 40),
-                                Stack(
-                                  children: [
-                                    Positioned(
-                                      top: 0,
-                                      left: 4,
-                                      bottom: 0,
-                                      child: Container(
-                                        width: 2,
-                                        decoration: const BoxDecoration(
-                                          color: AppColors.blueChill,
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color: AppColors.blueChill,
-                                              blurRadius: 4,
-                                              spreadRadius: 2,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 10),
-                                      child: Column(
-                                        children: const [
-                                          ItemWorkKnowledge(
-                                            title:
-                                                'Universidade Paranaense(UNIPAR) - 2015',
-                                            description:
-                                                '- Análise e Desenvolvimento de Sistemas',
-                                          ),
-                                          ItemWorkKnowledge(
-                                            title: 'Udemy - 2021',
-                                            description:
-                                                '- Flutter utilizando TDD; \n- Clean Architecture; \n- Design Patterns; \n- SOLID;',
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          )
-                              .animate(
-                                target: started ? 1.0 : 0.0,
-                                onPlay: (controller) {
-                                  if (started) {
-                                    controller.forward();
-                                  }
-                                },
-                              )
-                              .moveX(
-                                begin: 100,
-                                end: 0,
-                                duration: 1.seconds,
-                                curve: Curves.easeInOut,
-                              ),
-                        ],
-                      ),
+                          ],
+                        ),
                     ],
                   ),
                 ),
@@ -196,6 +100,129 @@ class Jobs extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+}
+
+class Knowledge extends StatelessWidget {
+  const Knowledge({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Aqui está o resumo de minha formação acadêmica e cursos.',
+          style: GoogleFonts.poppins(
+            color: Colors.white.withOpacity(0.7),
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(height: 40),
+        Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 4,
+              bottom: 0,
+              child: Container(
+                width: 2,
+                decoration: const BoxDecoration(
+                  color: AppColors.blueChill,
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.blueChill,
+                      blurRadius: 4,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Column(
+                children: const [
+                  ItemWorkKnowledge(
+                    title: 'Universidade Paranaense(UNIPAR) - 2015',
+                    description: '- Análise e Desenvolvimento de Sistemas',
+                  ),
+                  ItemWorkKnowledge(
+                    title: 'Udemy - 2021',
+                    description:
+                        '- Flutter utilizando TDD; \n- Clean Architecture; \n- Design Patterns; \n- SOLID;',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class Work extends StatelessWidget {
+  const Work({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Aqui está o resumo de minha experiência profissional.',
+          style: GoogleFonts.poppins(
+            color: Colors.white.withOpacity(0.7),
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(height: 40),
+        Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 4,
+              bottom: 0,
+              child: Container(
+                width: 2,
+                decoration: const BoxDecoration(
+                  color: AppColors.blueChill,
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.blueChill,
+                      blurRadius: 4,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Column(
+                children: const [
+                  ItemWorkKnowledge(
+                    title: 'Megaleios - 2019 até o momento',
+                    description:
+                        '- Desenvolvimento com Flutter; \n- Modular(gerência de dependências, rotas); \n- Bloc, RxDart, GetX; \n- Integração com Firebase; \n- Integração com OneSignal; \n- Figma; \n- Publicar App nas lojas(Apple e GooglePlay); \n- CD com Bitrise; \n- Integração com APIs REST; \n-Clean Architecture; \n-TDD; \n- Visionamento de código com git; \n- Desenvolvimento com React Native;',
+                  ),
+                  ItemWorkKnowledge(
+                    title: 'Sisterra - 2015 até 2019',
+                    description:
+                        '- Desenvolvimento de aplicativos para Android nativo (Java e Kotlin); \n- Desenvolvimento de aplicações em PHP utilizando DDD para back-end; \n- APIs REST; \n- CQRS pattern; \n- Conhecimento em Docker; \n- Publicar App nas lojas(Apple e GooglePlay); \n- Conhecimento em AWS (Lambda, SNS, SQS, SES, EC2, RDS, S3); \n- Integração com APIs REST; \n- Usuário Linux (GNU / Linux Ubuntu);',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
