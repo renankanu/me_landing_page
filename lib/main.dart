2b6e5f0a4108e2c1993b4cc76b6a1558f4b717d7
renankanu <renankanu@gmail.com>
2023-02-25 19:01:16 -0300

    feature: create integration with firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:url_strategy/url_strategy.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
