62e042724c005a41bceb0f2d360f4814c3b48f20
renankanu <renankanu@gmail.com>
2023-03-05 20:18:21 -0300

    feature: create copy code file

--- lib/pages/home/components/vs_code_container/content_file.dart
+++ lib/pages/home/components/vs_code_container/content_file.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_highlight/flutter_highlight.dart';
 import 'package:flutter_highlight/themes/dracula.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../../../model/file_config.dart';
+import '../../../../shared/app_colors.dart';
 
 class ContentFile extends StatelessWidget {
   const ContentFile({
@@ -17,20 +20,82 @@ class ContentFile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isCopied = ValueNotifier(false);
     return Expanded(
-      child: ListView(
-        controller: scrollController,
-        physics: const ClampingScrollPhysics(),
+      child: Stack(
         children: [
-          HighlightView(
-            fileConfig.fileContent,
-            language: fileConfig.typeFile.description,
-            theme: draculaTheme,
-            padding: const EdgeInsets.all(12),
-            textStyle: GoogleFonts.robotoMono(
-              fontSize: 12,
+          Container(
+            color: const Color(0xff282a36),
+            child: ListView(
+              controller: scrollController,
+              physics: const ClampingScrollPhysics(),
+              children: [
+                HighlightView(
+                  fileConfig.fileContent,
+                  language: fileConfig.typeFile.description,
+                  theme: draculaTheme,
+                  padding: const EdgeInsets.all(12),
+                  textStyle: GoogleFonts.robotoMono(
+                    fontSize: 12,
+                  ),
+                ),
+              ],
             ),
           ),
+          Positioned(
+            top: 20,
+            right: 20,
+            child: ValueListenableBuilder(
+              valueListenable: isCopied,
+              builder: (_, copied, __) => GestureDetector(
+                onTap: () {
+                  isCopied.value = true;
+                  Clipboard.setData(
+                    ClipboardData(text: fileConfig.fileContent),
+                  );
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color(0xff44475a),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                    border: copied
+                        ? const Border.fromBorderSide(
+                            BorderSide(
+                              color: AppColors.blueChill,
+                              width: 1,
+                            ),
+                          )
+                        : null,
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        copied ? Icons.check : Icons.copy,
+                        size: 16,
+                        color: copied ? AppColors.blueChill : null,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(copied ? 'Arquivo copiado' : 'Copy to Clipboard')
+                    ],
+                  ),
+                ).animate(
+                  onPlay: (controller) {
+                    if (copied) {
+                      controller.forward();
+                    } else {
+                      controller.reverse();
+                    }
+                  },
+                ).shake(),
+              ),
+            ),
+          )
         ],
       ),
     );
