73b5f0a43c17a9c13d9b0df33d4ffffde5161aac
renankanu <renankanu@gmail.com>
2023-03-06 21:26:23 -0300

    feature: add animation in title

--- lib/pages/home/components/config.dart
+++ lib/pages/home/components/config.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:me_landing_page/pages/home/components/vs_code_container/vs_code_container.dart';
-import 'package:visibility_detector/visibility_detector.dart';
 
 import '../../../shared/utils/app_responsive.dart';
 import '../../../shared/widgets/app_title_section.dart';
@@ -13,54 +12,38 @@ class Config extends StatelessWidget {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final padding = width * 0.03;
-    final start = ValueNotifier(false);
 
     return Container(
       padding: const EdgeInsets.only(top: 80),
-      child: VisibilityDetector(
-        key: const Key('config'),
-        onVisibilityChanged: (visibilityInfo) {
-          final visiblePercentage = visibilityInfo.visibleFraction * 100;
-          if (visiblePercentage > 20) {
-            start.value = true;
-          }
-        },
-        child: LayoutBuilder(
-          builder: (_, __) {
-            return ValueListenableBuilder(
-              valueListenable: start,
-              builder: (_, started, __) {
-                return Center(
-                  child: Container(
-                    constraints: const BoxConstraints(minWidth: 1800),
-                    padding: globalPadding(context, padding),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 20),
-                        AppTitleSection(
-                          title: 'Minha Config de Dev.',
-                          started: started,
-                        ),
-                        const SizedBox(height: 20),
-                        Text(
-                          'Aqui está algumas configurações que eu uso para desenvolver, makefile para automatizar algumas tarefas, settings do vscode para deixar o ambiente mais agradável, extensões que uso no vscode e configurações do git.',
-                          style: GoogleFonts.poppins(
-                            color: Colors.white.withOpacity(0.7),
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                          ),
-                        ),
-                        const SizedBox(height: 40),
-                        const VsCodeContainer()
-                      ],
+      child: LayoutBuilder(
+        builder: (_, __) {
+          return Center(
+            child: Container(
+              constraints: const BoxConstraints(minWidth: 1800),
+              padding: globalPadding(context, padding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
+                  const AppTitleSection(
+                    title: 'Minha Config de Dev.',
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Aqui está algumas configurações que eu uso para desenvolver, makefile para automatizar algumas tarefas, settings do vscode para deixar o ambiente mais agradável, extensões que uso no vscode e configurações do git.',
+                    style: GoogleFonts.poppins(
+                      color: Colors.white.withOpacity(0.7),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
                     ),
                   ),
-                );
-              },
-            );
-          },
-        ),
+                  const SizedBox(height: 40),
+                  const VsCodeContainer()
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/shared/widgets/app_title_section.dart
+++ lib/shared/widgets/app_title_section.dart
@@ -2,53 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:me_landing_page/shared/app_colors.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class AppTitleSection extends StatelessWidget {
   const AppTitleSection({
     Key? key,
     required this.title,
-    this.started = false,
   }) : super(key: key);
 
   final String title;
-  final bool started;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            color: AppColors.blueChill.withOpacity(0.4),
-            borderRadius: BorderRadius.circular(10),
-          ),
-        )
-            .animate(
-              target: started ? 1.0 : 0.0,
-              onPlay: (controller) {
-                if (!started) {
-                  controller.forward();
-                } else {
-                  controller.reverse();
-                }
-              },
+    final start = ValueNotifier(false);
+    return VisibilityDetector(
+      key: UniqueKey(),
+      onVisibilityChanged: (info) {
+        final percent = info.visibleFraction * 100;
+        if (percent > 20) {
+          start.value = true;
+        }
+      },
+      child: ValueListenableBuilder(
+        valueListenable: start,
+        builder: (_, started, __) => Stack(
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                color: AppColors.blueChill.withOpacity(0.4),
+                borderRadius: BorderRadius.circular(10),
+              ),
             )
-            .shake(curve: Curves.easeInOut, hz: 0.7, duration: 2000.ms)
-            .slideX(curve: Curves.easeOut, begin: -0.6, end: 0.0)
-            .slideY(curve: Curves.bounceOut, begin: -0.6, end: 0.0)
-            .rotate(curve: Curves.easeInOut, begin: 0.0, end: 0.15),
-        Text(
-          title,
-          style: GoogleFonts.poppins(
-            color: Colors.white,
-            fontSize: 40,
-            fontWeight: FontWeight.w700,
-            letterSpacing: 3,
-          ),
+                .animate(
+                  target: started ? 1.0 : 0.0,
+                  onPlay: (controller) {
+                    if (!started) {
+                      controller.forward();
+                    } else {
+                      controller.reverse();
+                    }
+                  },
+                )
+                .shake(curve: Curves.easeInOut, hz: 0.7, duration: 2000.ms)
+                .slideX(curve: Curves.easeOut, begin: -0.6, end: 0.0)
+                .slideY(curve: Curves.bounceOut, begin: -0.6, end: 0.0)
+                .rotate(curve: Curves.easeInOut, begin: 0.0, end: 0.15),
+            Text(
+              title,
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.w700,
+                letterSpacing: 3,
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
